53af1c4cde2f4d04a6d2d858ce9d02206cc10fe7
Rodrigo Quezada <roderik.ikana@gmail.com>
2017-05-18 11:44:17 -0500

    Create init.el

---
+++ init.el
@@ -0,0 +1,62 @@
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(require 'package) ;; You might already have this line
+(add-to-list 'package-archives
+             '("melpa" . "https://melpa.org/packages/"))
+(when (< emacs-major-version 24)
+  ;; For important compatibility libraries like cl-lib
+  (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
+(package-initialize) ;; You might already have this line
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (material-theme helm-fuzzier helm))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+
+(global-set-key (kbd "M-x") 'helm-M-x)
+
+
+(global-linum-mode 1)
+
+(setq linum-format "%d ")
+
+
+(require 'xt-mouse)
+(xterm-mouse-mode)
+(require 'mouse)
+(xterm-mouse-mode t)
+(defun track-mouse (e))
+
+(setq mouse-wheel-follow-mouse 't)
+
+
+(global-set-key (kbd "<mouse-4>") 'alternating-scroll-down-line)
+(global-set-key (kbd "<mouse-5>") 'alternating-scroll-up-line)
+
+(defvar alternating-scroll-down-next t)
+(defvar alternating-scroll-up-next t)
+
+(defun alternating-scroll-down-line ()
+  (interactive "@")
+    (when alternating-scroll-down-next
+;      (run-hook-with-args 'window-scroll-functions )
+      (scroll-down-line))
+    (setq alternating-scroll-down-next (not alternating-scroll-down-next)))
+
+(defun alternating-scroll-up-line ()
+  (interactive "@")
+    (when alternating-scroll-up-next
+;      (run-hook-with-args 'window-scroll-functions)
+      (scroll-up-line))
+    (setq alternating-scroll-up-next (not alternating-scroll-up-next)))
