adb1517ea3ba60b4e46d84e524703c303f33db02
Rodrigo Quezada <roderik.ikana@gmail.com>
2017-06-14 23:29:21 -0500

    Update init.el

--- init.el
+++ init.el
@@ -15,7 +15,16 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (material-theme helm-fuzzier helm))))
+ '(ansi-color-faces-vector
+   [default bold shadow italic underline bold bold-italic bold])
+ '(custom-enabled-themes (quote (darkokai)))
+ '(custom-safe-themes
+   (quote
+    ("98cc377af705c0f2133bb6d340bf0becd08944a588804ee655809da5d8140de6" "5dc0ae2d193460de979a463b907b4b2c6d2c9c4657b2e9e66b8898d2592e3de5" "70403e220d6d7100bae7775b3334eddeb340ba9c37f4b39c189c2c29d458543b" default)))
+ '(hl-sexp-background-color "#121212")
+ '(package-selected-packages
+   (quote
+    (pig-mode multiple-cursors hcl-mode powerline darkokai-theme yaml-mode projectile dockerfile-mode go-mode material-theme helm-fuzzier helm))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -28,6 +37,8 @@
 
 
 (global-linum-mode 1)
+
+(electric-pair-mode 1)
 
 (setq linum-format "%d ")
 
@@ -61,7 +72,19 @@
       (scroll-up-line))
     (setq alternating-scroll-up-next (not alternating-scroll-up-next)))
 
+
 (setq backup-directory-alist
-  `((".*" . ,temporary-file-directory)))
+      `((".*" . ,temporary-file-directory)))
 (setq auto-save-file-name-transforms
-  `((".*" ,temporary-file-directory t)))
+      `((".*" ,temporary-file-directory t)))
+
+(setq nxml-slash-auto-complete-flag t)
+
+(projectile-global-mode)
+
+(require 'powerline)
+(powerline-default-theme)
+
+(require 'multiple-cursors)
+
+(global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
